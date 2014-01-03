5b67d09e8251d1ee5465afbb029a24f194b012d0
Bryan Irace <bryan.irace@gmail.com>
2014-01-02 19:09:50 -0500

    Initial Commit

---
+++ BRYParseKeyboardNotification/BRYParseKeyboardNotification.h
@@ -0,0 +1,13 @@
+//
+//  BRYParseKeyboardNotification.h
+//  BRYParseKeyboardNotification
+//
+//  Created by Bryan Irace on 1/2/14.
+//  Copyright (c) 2014 Bryan Irace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRYParseKeyboardNotification : NSObject
+
+@end
---
+++ BRYParseKeyboardNotification/BRYParseKeyboardNotification.m
@@ -0,0 +1,13 @@
+//
+//  BRYParseKeyboardNotification.m
+//  BRYParseKeyboardNotification
+//
+//  Created by Bryan Irace on 1/2/14.
+//  Copyright (c) 2014 Bryan Irace. All rights reserved.
+//
+
+#import "BRYParseKeyboardNotification.h"
+
+@implementation BRYParseKeyboardNotification
+
+@end
